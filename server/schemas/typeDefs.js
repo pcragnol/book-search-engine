dd1478ca84161ee6ca92b46a324017e12ffb61f7
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-05-17 20:47:09 -0400

    Completed resolvers.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -37,8 +37,8 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
     saveBook(input: BookInput): User
     removeBook(bookId: String!): User
   }
