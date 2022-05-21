a27d833925a170636e150a7489c31b82727dfa0d
pcragnol <94771135+pcragnol@users.noreply.github.com>
2022-05-20 21:58:02 -0400

    Deleted redundant RESTful API files

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -59,8 +59,8 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData?.savedBooks?.length
-            ? `Viewing ${userData?.savedBooks?.length} saved ${userData?.savedBooks?.length === 1 ? 'book' : 'books'}:`
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
